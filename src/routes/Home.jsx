86faeaef66509e345d44d248060dc1ba6778eadc
vmhowley <vmhowleyh@gmail.com>
2024-10-16 16:49:10 -0400

    latest

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from '../components/Navbar'
+import NavBar from '../components/NavBar'
 import BrandsCarousel from '../components/BrandsCarousel'
 import ProductContainer from '../components/ProductContainer'
 import Navigation from  '../components/Navigation'
